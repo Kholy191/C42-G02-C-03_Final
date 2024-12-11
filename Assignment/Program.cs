40a815678d1a729cccad575371c7575b9ca37e1d
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-11 21:54:44 +0200

    Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -20,6 +20,42 @@
             //    Console.WriteLine("negative");
             #endregion
 
+            #region Q3 Write a program that takes 3 integers from the user then prints the max element and the min element.
+            //int Max = 0;
+            //int Min = 0;
+            //int.TryParse(Console.ReadLine(), out int Num1);
+            //int.TryParse(Console.ReadLine(), out int Num2);
+            //int.TryParse(Console.ReadLine(), out int Num3);
+            //if (Num1 > Num2 && Num1 > Num3)
+            //{
+            //    Max = Num1;
+            //    if (Num2 > Num3)
+            //        Min = Num3;
+            //    else
+            //        Min = Num2;
+            //}
+            //else if (Num2 > Num1 && Num2 > Num3)
+            //{
+            //    Max = Num2;
+            //    if (Num1 > Num3)
+            //        Min = Num3;
+            //    else
+            //        Min = Num1;
+            //}
+
+            //else
+            //{
+            //    Max = Num3;
+            //    if (Num1 > Num2)
+            //        Min = Num2;
+            //    else
+            //        Min = Num1;
+            //}
+
+            //Console.WriteLine($"Max = {Max}, Min = {Min}");
+
+            #endregion
+
         }
     }
 }
